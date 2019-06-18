a5d0e1f15f530ea1c5b5c5ba83ff747bd5e89e3c
zoman_hp <zomanhome@gmail.com>
2019-06-18 12:51:16 +0300

    new

--- 7/src/task7.js
+++ 7/src/task7.js
@@ -23,7 +23,7 @@ jsElementaryTasks.task7 = function(event, obj) {
 
     if (!isNumeric(min) || !isNumeric(max))
       return (obj1 = { status: 'error', reason: 'not a number' });
-    if (min > max) return (obj1 = { status: 'error', reason: 'min > max' });
+    if (+min > +max) return (obj1 = { status: 'error', reason: 'min > max' });
 
     return true;
   }
