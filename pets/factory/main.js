af97b8cae27257bbc2c21117c93ed85d7c814f00
zoman_hp <zomanhome@gmail.com>
2019-06-23 19:49:10 +0300

    factory fix2

--- pets/factory/main.js
+++ pets/factory/main.js
@@ -32,11 +32,25 @@
       <div class="card-body">
         <h5 class="card-title">${obj.name} [${obj.type}]</h5>
         <p class="card-text">Quantity: ${obj.quantity}</p>
-        <p class="card-text">Price: ${obj.price}</p>
+        <p class="card-text">Color: ${obj.color}</p>
+        <p class="card-text">Group: ${obj.group}</p>
+        <p class="card-text">Fur: ${obj.fur}</p>
+        <p class="card-text">Level: ${obj.level}</p>
+        <p class="card-text">Fly: ${obj.fly}</p>
+        <p class="card-text">Predator: ${obj.predator}</p>
+        <p class="card-text">Freshwater: ${obj.freshwater}</p>
+        <p class="card-text">Talkativeness: ${obj.talkativeness}</p>
+        <p class="card-text font-weight-bold">Price: ${obj.price}</p>
       </div>
     </div>`;
     });
     document.querySelector('.cards').innerHTML = cards;
+    document.querySelectorAll('.card-text').forEach(p => {
+      if (p.innerHTML.indexOf('undefined') !== -1) p.remove();
+      if (p.innerHTML.indexOf('false') !== -1) p.remove();
+      if (p.innerHTML.indexOf('true') !== -1)
+        p.innerHTML = p.innerHTML.replace(/: true/, '');
+    });
   }
 
   function getData(path, callback) {
