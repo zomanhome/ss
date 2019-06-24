b465449017663d3bcb9ac9c881f268a0e7f877f4
zoman_hp <zomanhome@gmail.com>
2019-06-24 09:50:25 +0300

    factory fetch

--- pets/factory/main.js
+++ pets/factory/main.js
@@ -1,34 +1,26 @@
 // Factory Method
-(function() {
-  function Animal(obj) {
-    this.id = obj.id;
-    this.name = obj.name;
-    this.price = obj.price;
-    this.quantity = obj.quantity;
-    this.age = obj.age;
-    this.type = obj.type;
-    this.gender = obj.gender;
-    this.weight = obj.weight;
-    this.color = obj.color;
-    this.lifespan = obj.lifespan;
-    this.predator = obj.predator;
-  }
-  function PetFactory() {}
+function PetFactory() {}
+
+(function init() {
   PetFactory.animals = [];
-  getData('db.json', createAnimals);
+  fetch('pets.json', {
+    method: 'GET'
+  })
+    .then(response => response.json())
+    .then(json => {
+      json.forEach(obj => {
+        PetFactory.animals.push(PetFactory.create(obj));
+      });
+      createCards(PetFactory.animals);
+    })
+    .catch(error => console.error(error));
+})();
 
-  function createAnimals(data) {
-    data.forEach(obj => {
-      PetFactory.animals.push(PetFactory.create(obj));
-    });
-    createCards(PetFactory.animals);
-  }
+function createCards(arr) {
+  let cards = '';
 
-  function createCards(arr) {
-    let cards = '';
-
-    arr.forEach(obj => {
-      cards += `<div class="card text-white bg-info mb-3" style="width: 18rem;">
+  arr.forEach(obj => {
+    cards += `<div class="card text-white bg-info mb-3" style="width: 18rem;">
       <div class="card-body">
         <h5 class="card-title">${obj.name} [${obj.type}]</h5>
         <p class="card-text">Quantity: ${obj.quantity}</p>
@@ -43,78 +35,75 @@
         <p class="card-text font-weight-bold">Price: ${obj.price}</p>
       </div>
     </div>`;
-    });
-    document.querySelector('.cards').innerHTML = cards;
-    document.querySelectorAll('.card-text').forEach(p => {
-      if (p.innerHTML.indexOf('undefined') !== -1) p.remove();
-      if (p.innerHTML.indexOf('false') !== -1) p.remove();
-      if (p.innerHTML.indexOf('true') !== -1)
-        p.innerHTML = p.innerHTML.replace(/: true/, '');
-    });
+  });
+  document.querySelector('.cards').innerHTML = cards;
+  document.querySelectorAll('.card-text').forEach(p => {
+    if (p.innerHTML.indexOf('undefined') !== -1) p.remove();
+    if (p.innerHTML.indexOf('false') !== -1) p.remove();
+    if (p.innerHTML.indexOf('true') !== -1)
+      p.innerHTML = p.innerHTML.replace(/: true/, '');
+  });
+}
+
+function Animal(obj) {
+  this.id = obj.id;
+  this.name = obj.name;
+  this.price = obj.price;
+  this.quantity = obj.quantity;
+  this.age = obj.age;
+  this.type = obj.type;
+  this.gender = obj.gender;
+  this.weight = obj.weight;
+  this.color = obj.color;
+  this.lifespan = obj.lifespan;
+  this.predator = obj.predator;
+}
+
+PetFactory.register = function(name, PetConstructor) {
+  if (name instanceof Function) {
+    PetConstructor = name;
+    name = null;
   }
+  if (!(PetConstructor instanceof Function)) {
+    throw {
+      name: 'Error',
+      message: 'PetConstructor is not function'
+    };
+  }
+  this[name || PetConstructor.name] = PetConstructor;
+};
+PetFactory.create = function(obj) {
+  let PetConstructor = this[obj.type];
 
-  function getData(path, callback) {
-    let xhr = new XMLHttpRequest();
-
-    xhr.open('GET', path, true);
-    xhr.send();
-    xhr.onreadystatechange = function() {
-      if (this.readyState != 4) return;
-      if (this.status != 200) {
-        console.log(
-          'ошибка: ' + (this.status ? this.statusText : 'запрос не удался')
-        );
-        return;
-      }
-      callback(JSON.parse(this.responseText));
+  if (!(PetConstructor instanceof Function)) {
+    throw {
+      name: `Error`,
+      message: `constructor ${obj.type} undefined`
     };
   }
 
-  PetFactory.register = function(name, PetConstructor) {
-    if (name instanceof Function) {
-      PetConstructor = name;
-      name = null;
-    }
-    if (!(PetConstructor instanceof Function)) {
-      throw {
-        name: 'Error',
-        message: 'PetConstructor is not function'
-      };
-    }
-    this[name || PetConstructor.name] = PetConstructor;
-  };
-  PetFactory.create = function(obj) {
-    var PetConstructor = this[obj.type];
-    if (!(PetConstructor instanceof Function)) {
-      throw {
-        name: 'Error',
-        message: 'constructor "' + obj.type + '" undefined'
-      };
-    }
-    return new PetConstructor(obj);
-  };
-
-  PetFactory.register('dog', function(obj) {
-    Animal.apply(this, arguments);
-    this.pedigree = obj.pedigree;
-    this.group = obj.group;
-  });
-  PetFactory.register('cat', function(obj) {
-    Animal.apply(this, arguments);
-    this.fur = obj.fur;
-    this.docked = obj.docked;
-    this.munchkin = obj.munchkin;
-    this.lopiness = obj.lopiness;
-  });
-  PetFactory.register('fish', function(obj) {
-    Animal.apply(this, arguments);
-    this.freshwater = obj.freshwater;
-    this.level = obj.level;
-  });
-  PetFactory.register('bird', function(obj) {
-    Animal.apply(this, arguments);
-    this.fly = obj.fly;
-    this.talkativeness = obj.talkativeness;
-    this.melodiousness = obj.melodiousness;
-  });
-})();
+  return new PetConstructor(obj);
+};
+PetFactory.register('dog', function(obj) {
+  Animal.apply(this, arguments);
+  this.pedigree = obj.pedigree;
+  this.group = obj.group;
+});
+PetFactory.register('cat', function(obj) {
+  Animal.apply(this, arguments);
+  this.fur = obj.fur;
+  this.docked = obj.docked;
+  this.munchkin = obj.munchkin;
+  this.lopiness = obj.lopiness;
+});
+PetFactory.register('fish', function(obj) {
+  Animal.apply(this, arguments);
+  this.freshwater = obj.freshwater;
+  this.level = obj.level;
+});
+PetFactory.register('bird', function(obj) {
+  Animal.apply(this, arguments);
+  this.fly = obj.fly;
+  this.talkativeness = obj.talkativeness;
+  this.melodiousness = obj.melodiousness;
+});
