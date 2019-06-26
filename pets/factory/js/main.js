8a0c17e36b0678a2d3ce2deb229aebc20db1bb8a
zoman_hp <zomanhome@gmail.com>
2019-06-26 12:39:18 +0300

    pets 20 cards

--- pets/factory/js/main.js
+++ pets/factory/js/main.js
@@ -11,32 +11,39 @@ function PetFactory() {}
       json.forEach(obj => {
         PetFactory.animals.push(PetFactory.create(obj));
       });
-      createCards(PetFactory.animals);
+      renderCards(PetFactory.animals);
     })
     .catch(error => console.error(error));
 })();
 
-function createCards(arr) {
-  let cards = '';
+function renderCards(arr) {
+  let cards = ``;
 
   arr.forEach(obj => {
-    cards += `<div class="card text-white bg-info mb-3" style="width: 18rem;">
-      <div class="card-body">
-        <h5 class="card-title">${obj.name} [${obj.type}]</h5>
-        <p class="card-text">Quantity: ${obj.quantity}</p>
-        <p class="card-text">Color: ${obj.color}</p>
-        <p class="card-text">Group: ${obj.group}</p>
-        <p class="card-text">Fur: ${obj.fur}</p>
-        <p class="card-text">Level: ${obj.level}</p>
-        <p class="card-text">Fly: ${obj.fly}</p>
-        <p class="card-text">Predator: ${obj.predator}</p>
-        <p class="card-text">Freshwater: ${obj.freshwater}</p>
-        <p class="card-text">Talkativeness: ${obj.talkativeness}</p>
-        <p class="card-text font-weight-bold">Price: ${obj.price}</p>
+    cards += `
+      <div class="card text-dark bg-light border-secondary">
+        <div class="card-header">${obj.name}</div>
+        <img src="img/id${obj.id}.jpg" class="card-img-top" alt="${obj.name}" />
+        <div class="card-body">
+          <h5 class="card-title text-right">$ ${obj.price}</h5>
+          <p class="card-text mb-1">Quantity: ${obj.quantity}</p>
+          <p class="card-text mb-1">Color: ${obj.color}</p>
+          <p class="card-text mb-1">Group: ${obj.group}</p>
+          <p class="card-text mb-1">Fur: ${obj.fur}</p>
+          <p class="card-text mb-1">Level: ${obj.level}</p>
+          <p class="card-text mb-1">Fly: ${obj.fly}</p>
+          <p class="card-text mb-1">Predator: ${obj.predator}</p>
+          <p class="card-text mb-1">Freshwater: ${obj.freshwater}</p>
+          <p class="card-text mb-1">Talkativeness: ${obj.talkativeness}</p>
+        </div>
+        <div class="card-footer">
+          <small class="text-muted">${obj.type}</small>
+        </div>
       </div>
-    </div>`;
+    `;
   });
-  document.querySelector('.cards').innerHTML = cards;
+  cards = `<div class="cards">${cards}</div>`;
+  document.querySelector('.container').innerHTML = cards;
   document.querySelectorAll('.card-text').forEach(p => {
     if (p.innerHTML.indexOf('undefined') !== -1) p.remove();
     if (p.innerHTML.indexOf('false') !== -1) p.remove();
